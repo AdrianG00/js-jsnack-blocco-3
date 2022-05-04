fe1762a44861eedefaeb708c289a9bf55a58e7d1
AdrianG00 <giurgicatalin@gmail.com>
2022-05-04 14:21:35 +0200

    Add: jsnack2

---
+++ snack2/js/script.js
@@ -0,0 +1,20 @@
+const array=[];
+let som=0;
+
+do{
+    let number = parseInt(prompt("Inserisci un numero"));
+    som += number;
+    if (som<50){
+        array.push(number);
+    }
+}while(som<50);
+
+
+if (som===50){
+    alert("la somma dei numeri inseriti è uguale a 50");
+    console.log(array); 
+}
+else{
+    alert("la somma dei numeri è maggiore di 50");
+    console.log(array);
+}
